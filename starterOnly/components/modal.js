86b21ec68a0f57f1dea281a74026079411b31cee
dlvga <dlvga@mail.com>
2025-01-27 21:36:16 +0100

    Fix closing modal issue #1 and html

--- starterOnly/components/modal.js
+++ starterOnly/components/modal.js
@@ -11,13 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseIcon = document.getElementById('modal__icon--close');
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+//close modal when click on close modal icon
+const closeModal =  () => {
+  modalbg.style.display = "none";
+}
+
+// event listener on modal close icon
+modalCloseIcon.addEventListener("click", closeModal);
 
 
